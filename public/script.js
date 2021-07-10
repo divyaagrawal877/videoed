f2d6d6659312d7c220439fc8651cc28bc397ca33
Divya Agrawal <dagrawal@dendritecaptialpartners.com>
2021-07-10 18:42:52 +0545

    deleted username from many places

--- public/script.js
+++ public/script.js
@@ -40,7 +40,7 @@ navigator.mediaDevices.getUserMedia({
         }
     });
     socket.on('createMessage',(message) => {
-        $('ul').append(`<li class="message"><b>${user__name}</b><br/>${message}</li>`);
+        $('ul').append(`<li class="message"><b>user__name</b><br/>${message}</li>`);
 
     })
 })
